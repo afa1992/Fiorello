7db1925354989a0f07bea4a8209c331288576549
afa1992 <afunaenigma34@gmail.com>
2023-01-15 22:41:30 +0400

    orange page modal added

--- src/app/app.js
+++ src/app/app.js
@@ -192,3 +192,16 @@ rangeInput.forEach(input=>{
         
     })
 })
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
